c495f314a348e46eab784be501fc8152b8a8216b
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2024-03-16 14:02:26 +0700

    update

--- leetcode_craft/binary_tree_helper.py
+++ leetcode_craft/binary_tree_helper.py
@@ -24,7 +24,7 @@ class TreeNode:
                 result.append(None)
 
         # Remove trailing nulls from the array representation
-        while result and result[-1] == None:
+        while result and result[-1] is None:
             result.pop()
 
         return str(result)
@@ -38,17 +38,23 @@ class TreeNode:
 def build_binary_tree_from_array_representation(nodes: list):
     if not nodes:
         return None
+
     root = TreeNode(nodes[0])
     queue = deque([root])
     i = 1
+
     while queue and i < len(nodes):
+
         current_node = queue.popleft()
+
         if nodes[i] is not None:
             current_node.left = TreeNode(nodes[i])
             queue.append(current_node.left)
         i += 1
+
         if i < len(nodes) and nodes[i] is not None:
             current_node.right = TreeNode(nodes[i])
             queue.append(current_node.right)
         i += 1
+
     return root
