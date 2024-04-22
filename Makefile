72b0161ed71a3363219c5ef00f92c773f8427a0b
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2024-04-22 09:41:50 +0700

    update test case

--- Makefile
+++ Makefile
@@ -3,4 +3,3 @@ lint:
 	poetry run isort .
 	poetry run ruff check .
 	poetry run mypy .
-	
--- tests/test_binary_tree_helper.py
+++ tests/test_binary_tree_helper.py
@@ -3,5 +3,4 @@ from leetcode_craft.binary_tree_helper import TreeNode
 
 def test_tree_node():
     node = TreeNode(val=5)
-    assert node.val = 5
-    pass
+    assert node.val == 5
